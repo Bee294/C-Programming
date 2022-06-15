6fb4b08ba760616fccdaae76c794851ffb2c61fd
Đỗ Thế Đức <93904318+Bee294@users.noreply.github.com>
2022-06-15 14:02:25 +0700

    Use Collections classes to work with set of Objects

---
+++ Lab04/Lab04/Program.cs
@@ -0,0 +1,16 @@
+﻿using Lab04;
+using System.Collections;
+
+ArrayList inv = new ArrayList();
+//Add elements to the list
+
+inv.Add(new Product("A", 5.9, 3));
+inv.Add(new Product("B", 8.2, 2));
+inv.Add(new Product("C", 3.5, 4));
+inv.Add(new Product("D", 1.8, 8));
+
+Console.WriteLine("Product List: ");
+foreach (Product p in inv)
+{
+    Console.WriteLine("" + p);
+}
